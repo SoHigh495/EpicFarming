3f9e0d47725b8769fa628d970e5a2396be294bd0
Sven Bledt <plenschin@live.de>
2022-10-04 00:45:54 +0200

    Fix Common Lang missing error
    Update Common-lang3

--- src/main/java/com/songoda/epicfarming/EpicFarming.java
+++ src/main/java/com/songoda/epicfarming/EpicFarming.java
@@ -45,7 +45,7 @@ import com.songoda.epicfarming.utils.EntityUtils;
 import com.songoda.epicfarming.utils.Methods;
 import com.songoda.skyblock.SkyBlock;
 import com.songoda.skyblock.permission.BasicPermission;
-import org.apache.commons.lang.math.NumberUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
